004fda53d98ca951a602ce365e8134710bc88e00
a <suculent@me.com>
2018-04-22 20:23:35 +0200

    added clusterization by CPU count

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ var Transformer = function() {
   const server_id = uuidv1();
   var server_mac = null;
 
+  const cluster = require('cluster');
+  const numCPUs = require('os').cpus().length; // default number of forks
+
   require('getmac').getMac(function(err, macAddress) {
     if (err) {
       console.log("getmac error: " + err);
@@ -22,164 +25,186 @@ var Transformer = function() {
     }
   });
 
-  var Rollbar = require("rollbar");
-
-  var rollbar = new Rollbar({
-    accessToken: "5505bac5dc6c4542ba3bd947a150cb55",
-    handleUncaughtExceptions: true,
-    handleUnhandledRejections: true
-  });
-
-  function respond(res, object) {
-    if (typeOf(object) == "buffer") {
-      res.header("Content-Type", "application/octet-stream");
-      res.send(object);
-    } else if (typeOf(object) == "string") {
-      res.end(object);
-    } else {
-      res.end(JSON.stringify(object));
+  if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+    // Fork workers.
+    const forks = numCPUs;
+    for (let i = 0; i < forks; i++) {
+      cluster.fork();
     }
-  }
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`worker ${worker.process.pid} died`);
+    });
+  } else {
+    // Workers can share any TCP connection
+    // In this case it is an HTTP server
+    http.createServer((req, res) => {
+      res.writeHead(200);
+      res.end('hello world\n');
+    }).listen(8000);
 
-  console.log("Starting THiNX Transformer Server Node");
+    console.log(`Worker ${process.pid} started`);
 
-  var app = express();
+    var Rollbar = require("rollbar");
 
-  app.use(parser.json({
-    limit: "1mb"
-  }));
+    var rollbar = new Rollbar({
+      accessToken: "5505bac5dc6c4542ba3bd947a150cb55",
+      handleUncaughtExceptions: true,
+      handleUnhandledRejections: true
+    });
 
-  app.use(parser.urlencoded({
-    extended: true,
-    parameterLimit: 1000,
-    limit: "1mb"
-  }));
-
-  const http_port = 7474;
-
-  http.createServer(app).listen(http_port);
-
-  console.log("Started on port: " + http_port);
-
-  app.all("/*", function(req, res, next) {
-
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-type,Accept,X-Access-Token,X-Key");
-
-    if (req.method == "OPTIONS") {
-      res.status(200).end();
-    } else {
-      next();
-    }
-  });
-
-  //
-  // Handlers
-  //
-
-  app.post("/do", function(req, res) {
-
-    //
-    // Input validation
-    //
-    //
-
-    if (typeof(req.body) === "undefined") {
-      respond(res, {
-	      success: false,
-	      error: "missing: body"
-	    });;
-      return;
-    }
-
-    var ingress = req.body;
-
-    try {
-      ingress = JSON.parse(req.body);
-    } catch (e) {
-      ingress = req.body;
-    }
-
-    console.log("Ingress: " + ingress);
-    console.log("Ingress.json: " + JSON.stringify(ingress));
-
-    var jobs = ingress.jobs;
-
-    console.log("Jobs: " + JSON.stringify(jobs));
-
-    if (typeof(ingress.jobs) === "undefined") {
-      respond(res, {
-	      success: false,
-	      error: "missing: body.jobs"
-	    });;
-      return;
-    }
-
-    //
-    // Run loop
-    //
-
-    var input_raw = jobs[0].params.status;
-    console.log("Input: " + input_raw);
-
-    var status = input_raw;
-    var error = null;
-
-    for (job_index in jobs) {
-
-      const job = jobs[job_index];
-      const code = job.code;
-      const owner = job.owner;
-      const transaction_id = job.id;
-
-      try {
-
-        var exec = null;
-
-        /* jshint -W061 */
-        var cleancode;
-        try {
-          cleancode = unescape(base64.decode(code));
-        } catch (e) {
-          cleancode = unescape(code); // accept bare code for testing, will deprecate
-        }
-
-        console.log("Evaluating code: "+cleancode);
-
-        eval(cleancode); // expects transformer(status, device); function only; may provide API
-
-        console.log("Running job:");
-        console.log("- codename: "+job.codename);
-        console.log("- id: "+job.id);
-        console.log("- params: "+JSON.stringify(job.params));
-        console.log("- owner: "+JSON.stringify(job.owner));
-
-        status = transformer(status, job.params.device); // passthrough previous status
-        console.log("Transformed status: '" + status + "'");
-        /* jshint +W061 */
-      } catch (e) {
-        console.log(e);
-        error = JSON.stringify(e);
+    function respond(res, object) {
+      if (typeOf(object) == "buffer") {
+        res.header("Content-Type", "application/octet-stream");
+        res.send(object);
+      } else if (typeOf(object) == "string") {
+        res.end(object);
+      } else {
+        res.end(JSON.stringify(object));
       }
     }
 
-    respond(res, {
-      input: input_raw,
-      output: status,
-      error: error
+    console.log("Starting THiNX Transformer Server Node");
+
+    var app = express();
+
+    app.use(parser.json({
+      limit: "1mb"
+    }));
+
+    app.use(parser.urlencoded({
+      extended: true,
+      parameterLimit: 1000,
+      limit: "1mb"
+    }));
+
+    const http_port = 7474;
+
+    http.createServer(app).listen(http_port);
+
+    console.log("Started on port: " + http_port);
+
+    app.all("/*", function(req, res, next) {
+
+      res.header("Access-Control-Allow-Credentials", "true");
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+      res.header("Access-Control-Allow-Headers", "Content-type,Accept,X-Access-Token,X-Key");
+
+      if (req.method == "OPTIONS") {
+        res.status(200).end();
+      } else {
+        next();
+      }
     });
 
-  });
+    //
+    // Handlers
+    //
 
-  /* Credits handler, returns current credits from user info */
-  app.get("/id", function(req, res) {
-    respond(res, {
-      id: server_id,
-      mac: server_mac
+    app.post("/do", function(req, res) {
+
+      //
+      // Input validation
+      //
+      //
+
+      if (typeof(req.body) === "undefined") {
+        respond(res, {
+  	      success: false,
+  	      error: "missing: body"
+  	    });;
+        return;
+      }
+
+      var ingress = req.body;
+
+      try {
+        ingress = JSON.parse(req.body);
+      } catch (e) {
+        ingress = req.body;
+      }
+
+      console.log("Ingress: " + ingress);
+      console.log("Ingress.json: " + JSON.stringify(ingress));
+
+      var jobs = ingress.jobs;
+
+      console.log("Jobs: " + JSON.stringify(jobs));
+
+      if (typeof(ingress.jobs) === "undefined") {
+        respond(res, {
+  	      success: false,
+  	      error: "missing: body.jobs"
+  	    });;
+        return;
+      }
+
+      //
+      // Run loop
+      //
+
+      var input_raw = jobs[0].params.status;
+      console.log("Input: " + input_raw);
+
+      var status = input_raw;
+      var error = null;
+
+      for (job_index in jobs) {
+
+        const job = jobs[job_index];
+        const code = job.code;
+        const owner = job.owner;
+        const transaction_id = job.id;
+
+        try {
+
+          var exec = null;
+
+          /* jshint -W061 */
+          var cleancode;
+          try {
+            cleancode = unescape(base64.decode(code));
+          } catch (e) {
+            cleancode = unescape(code); // accept bare code for testing, will deprecate
+          }
+
+          console.log("Evaluating code: "+cleancode);
+
+          eval(cleancode); // expects transformer(status, device); function only; may provide API
+
+          console.log("Running job:");
+          console.log("- codename: "+job.codename);
+          console.log("- id: "+job.id);
+          console.log("- params: "+JSON.stringify(job.params));
+          console.log("- owner: "+JSON.stringify(job.owner));
+
+          status = transformer(status, job.params.device); // passthrough previous status
+          console.log("Transformed status: '" + status + "'");
+          /* jshint +W061 */
+        } catch (e) {
+          console.log(e);
+          error = JSON.stringify(e);
+        }
+      }
+
+      respond(res, {
+        input: input_raw,
+        output: status,
+        error: error
+      });
+
     });
-  });
+
+    /* Credits handler, returns current credits from user info */
+    app.get("/id", function(req, res) {
+      respond(res, {
+        id: server_id,
+        mac: server_mac
+      });
+    });
+
+  }
 
 };
 
